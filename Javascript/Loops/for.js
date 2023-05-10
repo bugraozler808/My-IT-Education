abedcf79f24622f45a8fc3c6e070b88300a84640
bugraozler <bugraozler808@gmail.com>
2023-05-10 21:29:57 +0300

    first

---
+++ Javascript/Loops/for.js
@@ -0,0 +1,31 @@
+// * =======================================================
+// *             FOR LOOP
+// * =======================================================
+
+console.log(" **** FOR ****")
+ 
+for(let i = 1 ;  i < 100 ; i++){
+
+    console.log("Cohort 14");
+}
+
+for(let i = 1; i<100; i*=2 ){
+    console.log(`${i}-FS14`);
+}
+// for(let i = 1; i<100; i*=2 ){
+//     console.log(`${i}-FS14`);
+// }
+// 1-FS14
+// for.js:13 2-FS14
+// for.js:13 4-FS14
+// for.js:13 8-FS14
+// for.js:13 16-FS14
+// for.js:13 32-FS14
+// for.js:13 64-FS14
+
+//? 1 DEN N KADAR SAYILARI TOPLAYAN KODU YAZINIZ.
+
+
+
+
+

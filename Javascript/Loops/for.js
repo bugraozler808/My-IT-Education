3c7d699889cea5cba9951dcde2a1145d521101d8
bugraozler <bugraozler808@gmail.com>
2023-05-10 21:30:08 +0300

    one commit

--- Javascript/Loops/for.js
+++ Javascript/Loops/for.js
@@ -15,13 +15,13 @@ for(let i = 1; i<100; i*=2 ){
 // for(let i = 1; i<100; i*=2 ){
 //     console.log(`${i}-FS14`);
 // }
-// 1-FS14
-// for.js:13 2-FS14
-// for.js:13 4-FS14
-// for.js:13 8-FS14
-// for.js:13 16-FS14
-// for.js:13 32-FS14
-// for.js:13 64-FS14
+//  1-FS14
+//  2-FS14
+//  4-FS14
+//  8-FS14
+//  16-FS14
+//  32-FS14
+//  64-FS14
 
 //? 1 DEN N KADAR SAYILARI TOPLAYAN KODU YAZINIZ.
 

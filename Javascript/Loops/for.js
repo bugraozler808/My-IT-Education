e960e6ab741115b38a74f388459f074ff3c56bdf
bugraozler <bugraozler808@gmail.com>
2023-06-05 23:25:59 +0300

    son

--- Javascript/Loops/for.js
+++ Javascript/Loops/for.js
@@ -2,15 +2,14 @@
 // *             FOR LOOP
 // * =======================================================
 
-console.log(" **** FOR ****")
- 
-for(let i = 1 ;  i < 100 ; i++){
+console.log(" **** FOR ****");
 
-    console.log("Cohort 14");
+for (let i = 1; i < 100; i++) {
+  console.log("Cohort 14");
 }
 
-for(let i = 1; i<100; i*=2 ){
-    console.log(`${i}-FS14`);
+for (let i = 1; i < 100; i *= 2) {
+  console.log(`${i}-FS14`);
 }
 // for(let i = 1; i<100; i*=2 ){
 //     console.log(`${i}-FS14`);
@@ -25,23 +24,20 @@ for(let i = 1; i<100; i*=2 ){
 
 //? 1 DEN N KADAR SAYILARI TOPLAYAN KODU YAZINIZ.
 
-const n = prompt("enter your number")
-let sum = 0 
-for(let i = 1; i <= n; i++ ){
-
-    
-   sum = sum + i
-    console.log(`sum: ${sum}`);
+const n = prompt("enter your number");
+let sum = 0;
+for (let i = 1; i <= n; i++) {
+  sum = sum + i;
+  console.log(`sum: ${sum}`);
 }
 
 // 0-100 arasında n adet rasgele tamsayı üreten kodu yazınız.
 
-const number = prompt("how many numbers")
-// Math.random() 0-1 arasında rasgele sayı üretir
-for(let sayac =1; sayac <= number; sayac++){
-
-    const rasgelesayı = Math.round( Math.random() *100)
-    console.log(`${sayac}. sayınız : ${rasgelesayı}`);
+const number = prompt("how many numbers");
+// Math.random() 0-1 arasında rasgele sayı uretir
+for (let sayac = 1; sayac <= number; sayac++) {
+  const rasgelesayı = Math.round(Math.random() * 100);
+  console.log(`${sayac}. sayınız : ${rasgelesayı}`);
 }
 
 //* NOT:
@@ -49,7 +45,3 @@ for(let sayac =1; sayac <= number; sayac++){
 //* Math.ceil() =>en yaki ust tamsayiya yuvarlar
 //* Math.round() => degerine gore yuvarlar.
 //* Math.trunc() => kesirli sayinin tam kismini alir.
-
-
-
-

4fb9156734366e7e07bd280bbbe5755ee925b206
bugraozler <bugraozler808@gmail.com>
2023-06-14 21:36:22 +0300

    setTimeout

--- Javascript/Async-1/intro.js
+++ Javascript/Async-1/intro.js
@@ -61,3 +61,16 @@
 //* Eger birbirine bagimli asenkron kodlarin yazilmasi gerekirse,nested callback
 //* yapisinin kullanilmasi gerekebilir. Fakat bu iyi bir programlama yaklasimi degildir.
 // !callback hell olarak adlandirilan bu yapinin anlasilmasi ve surdurulebilirligi oldukca zordur.
+
+setTimeout(() => {
+  console.log("john:Hi");
+  setTimeout(() => {
+    console.log("Sarah: Hello");
+    setTimeout(() => {
+      console.log("John: How Are you?");
+      setTimeout(() => {
+        console.log("Sarah:Fine and you?");
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);

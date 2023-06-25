fea873813e146918babee5b9c1d1374e03222af6
bugraozler <bugraozler808@gmail.com>
2023-06-25 18:33:00 +0300

    primitive

--- Javascript/Arrays/arrays.js
+++ Javascript/Arrays/arrays.js
@@ -13,3 +13,26 @@ console.log(colors[1]);
 console.log(colors[2]);
 console.log(colors[colors.length - 2]);
 // sondan ikinci elemanı getir
+
+// Array is Array
+
+const colorsone = ["RED", "GREEN", "BLUE"];
+console.log(typeof colorsone);
+
+console.log(Array.isArray(colorsone));
+console.log(colors instanceof Array);
+
+const variables = "1";
+console.log(Array.isArray(variables));
+console.log(variables instanceof Array);
+
+let str = "Brown";
+str[0] = "b";
+console.log(str);
+
+const change = ["BLUE", "GREEN", "BROWN"];
+change[1] = "gray";
+console.log(change);
+let a = "PINK";
+change[3] = a;
+console.log(change);
